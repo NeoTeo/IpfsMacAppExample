83d11446fbfa5dd50d35a0de5db866654bab761c
Teo Sartori <m@sartori.dk>
2018-01-04 10:27:21 +0100

    Update for Swift 4

--- IpfsApp/Document.swift
+++ IpfsApp/Document.swift
@@ -17,7 +17,7 @@ class Document: NSDocument {
         // Add your subclass-specific initialization here.
     }
 
-    override func windowControllerDidLoadNib(aController: NSWindowController) {
+    override func windowControllerDidLoadNib(_ aController: NSWindowController) {
         super.windowControllerDidLoadNib(aController)
         // Add any code here that needs to be executed once the windowController has loaded the document's window.
         do {
@@ -27,39 +27,28 @@ class Document: NSDocument {
                 (idData : JsonType) in
                 
                 let winName = idData.object?["ID"]?.string
-                
-                self.IpfsNodeId.stringValue = winName!
+    
+                DispatchQueue.main.async {
+                    self.IpfsNodeId.stringValue = winName!
+                }
             }
             
         } catch {
-            print("There was an error initializing the IPFS api client: \(error)")
+            Swift.print("There was an error initializing the IPFS api client: \(error)")
         }
 
     }
 
-    override class func autosavesInPlace() -> Bool {
-        return true
-    }
+    override class var autosavesInPlace: Bool { return true }
 
-    override var windowNibName: String? {
-        // Returns the nib file name of the document
-        // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this property and override -makeWindowControllers instead.
-        return "Document"
-    }
-
-    override func dataOfType(typeName: String) throws -> NSData {
-        // Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
-        // You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
+    override var windowNibName: NSNib.Name? { return NSNib.Name(rawValue: "Document") }
+    
+    override func data(ofType typeName: String) throws -> Data {
         throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
     }
 
-    override func readFromData(data: NSData, ofType typeName: String) throws {
-        // Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
-        // You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
-        // If you override either of these, you should also override -isEntireFileLoaded to return false if the contents are lazily loaded.
+    override func read(from data: Data, ofType typeName: String) throws {
         throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
     }
-
-
 }
 
